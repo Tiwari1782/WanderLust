d065282b9cfaff7176c4b12174b8740ec0673f48
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-02 17:25:32 +0530

    Made the listings path

---
+++ README.md
@@ -0,0 +1,9 @@
+# Model : Listing
+Place can be apartment, flat, house, villa, hotel, motel etc..
+
+->Title - String type
+->description - String type
+->image - URL
+->price - number
+->loaction - String type
+->country - String type
---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+let port = 8080;
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hii, I am root!!");
+});
+app.listen(port, () => {
+  console.log(`Server is Listening to port : ${port}`);
+});
