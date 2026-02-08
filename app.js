8e17da441b119beb963154bb4b1aba1651daa1a9
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-08 13:06:35 +0530

    Delete review route added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { wrap } = require("module");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 const session = require("express-session");
 let port = 8080;
@@ -31,7 +31,6 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 //Server
 app.get("/", (req, res) => {
   res.send("Hii, I am root!!");
@@ -40,6 +39,16 @@ app.get("/", (req, res) => {
 //Validate Listing
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+//Validate Review
+const validateReview = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
   if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
@@ -63,7 +72,7 @@ app.get("/listings/new", (req, res) => {
 
 //Create Route
 app.post(
-  "/listings", 
+  "/listings",
   validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
@@ -77,7 +86,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -103,7 +112,6 @@ app.put(
   }),
 );
 
-
 //Delete Route
 app.delete(
   "/listings/:id",
@@ -115,22 +123,38 @@ app.delete(
   }),
 );
 //Add Reviews
-//Post route 
-app.post("/listings/:id/reviews", async (req,res)=>{
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+//Post route
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
 
-  listing.reviews.push(newReview);
+    listing.reviews.push(newReview);
 
-  await newReview.save();
-  await listing.save();
+    await newReview.save();
+    await listing.save();
 
-  // console.log("New review saved");
-  // res.send("New review saved");
+    // console.log("New review saved");
+    // res.send("New review saved");
 
-  res.send(`/listings/${listing._id}`);
-});
+    // res.send(`/listings/${listing._id}`);
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
+//Delete Reviews
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
 
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  }),
+);
 // Privacy and Terms routes
 app.get("/privacy", (req, res) => {
   res.render("listings/privacy.ejs");
