2469e414cef653524c39ba545da7bc074ecc7531
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-08 13:05:52 +0530

    Added a middleware to delete review if a listing is deleted

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const Review = require("./review.js");
 const listingSchema = new Schema({
   title: {
     type: String,
@@ -22,10 +22,15 @@ const listingSchema = new Schema({
   reviews: [
     {
       type: Schema.Types.ObjectId,
-      ref:"Review",
+      ref: "Review",
     },
   ],
 });
 
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
