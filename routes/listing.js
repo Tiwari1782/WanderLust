f349f64cbe6bb9c2e1b611f0943b3ee716adb0a8
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-11 19:13:43 +0530

    Added flash notification and toast notification

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,6 @@ const { listingSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
 
-
 //Validate Listing
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
@@ -38,6 +37,7 @@ router.post(
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
   }),
 );
@@ -48,6 +48,12 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    
+    if (!listing) {
+      req.flash("error", "Listing not found!");
+      return res.redirect("/listings");
+    }
+    
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -58,6 +64,12 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    
+    if (!listing) {
+      req.flash("error", "Listing not found!");
+      return res.redirect("/listings");
+    }
+    
     res.render("listings/edit.ejs", { listing });
   }),
 );
@@ -69,6 +81,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   }),
 );
@@ -80,6 +93,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   }),
 );
