33a4d1d5f5312a2d0c5ed416ae7256fbef57327f
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-12 23:11:50 +0530

    Added updated paths and middlewares for post login and signup routes

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { savedRedirectUrl } = require("../middleware.js");
 
 //GET - Signup
 router.get("/signup", (req, res) => {
@@ -17,8 +18,13 @@ router.post(
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
       console.log(registeredUser);
-      req.flash("success", "Welcome to WanderLust");
-      res.redirect("/listings");
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
+        req.flash("success", "Welcome to WanderLust");
+        res.redirect("/listings");
+      });
     } catch (e) {
       req.flash("error", "User already exists");
       res.redirect("/signup");
@@ -33,13 +39,25 @@ router.get("/login", (req, res) => {
 //POST Route
 router.post(
   "/login",
+  savedRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  (async (req, res) => {
-    req.flash("success","Welcome to WanderLust! You are logged in!!")
-    res.redirect("/listings");
-  }),
+  async (req, res) => {
+    req.flash("success", "Welcome to WanderLust! You are logged in!!");
+    let redirectUrl = res.locals.redirectUrl || "/listings"
+    res.redirect(redirectUrl);
+  },
 );
+//Logout
+router.get("/logout", (req, res) => {
+  req.logOut((err) => {
+    if (err) {
+      next(err);
+    }
+    req.flash("success", "You are now Logged Out");
+    res.redirect("/listings");
+  });
+});
 module.exports = router;
