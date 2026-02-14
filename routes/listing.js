5a60cb9f8738aa4f6b8b0b9580772fef681d947a
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-14 23:51:11 +0530

    Create Listing path updated

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({storage});
 
 //Index and Create route
 router
@@ -11,14 +14,9 @@ router
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
+    upload.single('listing[image]'),
     validateListing,
-    wrapAsync(async (req, res, next) => {
-      const newListing = new Listing(req.body.listing);
-      newListing.owner = req.user._id;
-      await newListing.save();
-      req.flash("success", "New Listing Created!");
-      res.redirect("/listings");
-    }),
+    wrapAsync(listingController.createListing),
   );
 
 //New route
