1cbe194ecc882d9b1e89a2bac0251a6a38d42b52
Prakash Tiwari <prakashtiwarie06@gmail.com>
2026-02-17 18:57:23 +0530

    Added the map API

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,9 @@
 const Listing = require("../models/listing.js");
+const mbxStyles = require("@mapbox/mapbox-sdk/services/styles");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const { query } = require("express");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 //Index controller
 module.exports.index = async (req, res) => {
@@ -14,19 +19,38 @@ module.exports.renderNewForm = (req, res) => {
 
 //Create Listing
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    })
+    .send();
+
+  if (!response.body.features.length) {
+    req.flash("error", "Location not found! Please enter a valid location.");
+    return res.redirect("/listings/new");
+  }
+
   let url = req.file.path;
   let filename = req.file.filename;
+
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
   newListing.image = { url, filename };
+
+  newListing.geometry = response.body.features[0].geometry;
+
   await newListing.save();
+
   req.flash("success", "New Listing Created!");
   res.redirect("/listings");
 };
 
-//Show Listing controller
+
+// Show Listing controller
 module.exports.showListing = async (req, res) => {
   let { id } = req.params;
+
   const listing = await Listing.findById(id)
     .populate({
       path: "reviews",
@@ -40,8 +64,11 @@ module.exports.showListing = async (req, res) => {
     req.flash("error", "Listing not found!");
     return res.redirect("/listings");
   }
-  console.log(listing);
-  res.render("listings/show.ejs", { listing });
+
+  res.render("listings/show.ejs", {
+    listing,
+    mapToken: process.env.MAP_TOKEN,
+  });
 };
 
 //Search route controller
@@ -75,17 +102,46 @@ module.exports.renderEditForm = async (req, res) => {
   res.render("listings/edit.ejs", { listing });
 };
 
-//Update listing controller
+//Update Listing Controller
 module.exports.updateListing = async (req, res) => {
   let { id } = req.params;
-  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
 
-  if (typeof req.file != "undefined") {
+  // 1) Find listing first
+  let listing = await Listing.findById(id);
+
+  // 2) If location changed, re-geocode
+  if (req.body.listing.location !== listing.location) {
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: `${req.body.listing.location}, ${req.body.listing.country}`,
+        limit: 1,
+      })
+      .send();
+
+    if (!response.body.features.length) {
+      req.flash("error", "Location not found! Please enter a valid location.");
+      return res.redirect(`/listings/${id}/edit`);
+    }
+
+    listing.geometry = response.body.features[0].geometry;
+  }
+
+  // 3) Update all fields manually
+  listing.title = req.body.listing.title;
+  listing.description = req.body.listing.description;
+  listing.price = req.body.listing.price;
+  listing.location = req.body.listing.location;
+  listing.country = req.body.listing.country;
+
+  // 4) Update image if new file uploaded
+  if (typeof req.file !== "undefined") {
     let url = req.file.path;
     let filename = req.file.filename;
     listing.image = { url, filename };
-    await listing.save();
   }
+
+  // 5) Save
+  await listing.save();
 
   req.flash("success", "Listing Updated!");
   res.redirect(`/listings/${id}`);
